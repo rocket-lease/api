680626999be97912711c6d4b923a953e7f48c905
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2026-05-10 22:20:57 -0300

    feat(auth): register user (US-01)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from '@/infrastructure/modules/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
 })
 export class AppModule {}
